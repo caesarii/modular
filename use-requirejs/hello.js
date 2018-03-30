ebf961091480d4eca99518552bd07e7e1ed2cd11
caesarii <iwangqinghe@gmail.com>
2018-03-30 10:07:34 +0800

    阻塞式模块加载

---
+++ use-requirejs/hello.js
@@ -0,0 +1,14 @@
+//
+// define(function() {
+//     function sayHello() {
+//         alert('Hello world!')
+//     }
+//
+//     sayHello()
+// })
+
+function sayHello() {
+    alert('Hello world!')
+}
+
+sayHello()
