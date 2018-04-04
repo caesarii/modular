332cc1376843fa266d344d1c819367f4e489a3f6
caesarii <iwangqinghe@gmail.com>
2018-04-04 09:48:38 +0800

    update

--- use-seajs/modules/sea-debug.js
+++ use-seajs/modules/sea-debug.js
@@ -7,12 +7,14 @@
         return
     }
     
-    const seajs = global.seajs = {
+    const seajs = {
         // The current version of Sea.js being used
         version: '2.2.3'
     }
     
-    const data = seajs.data = {}
+    
+    const data =  {}
+    seajs.data = data
     
     /**
      * util-lang.js - The minimal language enhancement
@@ -39,7 +41,8 @@
      * util-events.js - The minimal events support
      */
     
-    var events = data.events = {}
+    var events = {}
+    data.events = events
     
     // Bind event
     seajs.on = function (name, callback) {
@@ -879,421 +882,17 @@
         // 模块执行完成
         EXECUTED: 6
     }
-    // function Module (uri, deps) {
-    //     this.uri = uri
-    //     this.dependencies = deps || []
-    //     this.exports = null
-    //     this.status = 0
-    //
-    //     // 依赖当前模块的模块
-    //     this._waitings = {}
-    //
-    //     // 未加载的依赖数
-    //     this._remain = 0
-    // }
-    //
-    // // 获取模块的依赖列表
-    // Module.prototype.resolve = function () {
-    //     const mod = this
-    //     const ids = mod.dependencies
-    //     const uris = []
-    //
-    //     for (let i = 0, len = ids.length; i < len; i++) {
-    //         uris[i] = Module.resolve(ids[i], mod.uri)
-    //     }
-    //     return uris
-    // }
-    //
-    // // Load module.dependencies and fire onload when all done
-    // Module.prototype.load = function () {
-    //
-    //     const mod = this
-    //
-    //     // 如果模块已经加载, 只需要等待 onload 调用
-    //     if (mod.status >= STATUS.LOADING) {
-    //         return
-    //     }
-    //
-    //     // 更新为 loading 状态
-    //     mod.status = STATUS.LOADING
-    //
-    //     // 获取当前模块的依赖列表
-    //     var uris = mod.resolve()
-    //     // Emit `load` event for plugins such as combo plugin
-    //     emit('load', uris)
-    //
-    //     mod._remain = uris.length
-    //     var len = uris.length
-    //     var m
-    //
-    //     // Initialize modules and register waitings
-    //     // 处理所有依赖模块
-    //     for (let i = 0; i < len; i++) {
-    //         m = Module.get(uris[i])
-    //         if (m.status < STATUS.LOADED) {
-    //             //TODO  如果模块未加载, 说明该模块依赖当前模块 ?
-    //             // Maybe duplicate: When module has dupliate dependency, it should be it's count, not 1
-    //             m._waitings[mod.uri] = (m._waitings[mod.uri] || 0) + 1
-    //         } else {
-    //             // 如果模块已加载
-    //             mod._remain--
-    //         }
-    //     }
-    //
-    //
-    //     if (mod._remain === 0) {
-    //         // 如果全部依赖已加载, 则调用 onload
-    //         mod.onload()
-    //         return
-    //     } else {
-    //         // 加载未加载的依赖
-    //         // Begin parallel loading
-    //         var requestCache = {}
-    //
-    //         for (let i = 0; i < len; i++) {
-    //             m = cachedMods[uris[i]]
-    //
-    //             if (m.status < STATUS.FETCHING) {
-    //                 // fetch
-    //                 // fetch 会修改 requestCache
-    //                 m.fetch(requestCache)
-    //             } else if (m.status === STATUS.SAVED) {
-    //                 // load
-    //                 m.load()
-    //             }
-    //         }
-    //
-    //         // Send all requests at last to avoid cache bug in IE6-9. Issues#808
-    //         // 发送请求
-    //         for (var requestUri in requestCache) {
-    //             if (requestCache.hasOwnProperty(requestUri)) {
-    //                 requestCache[requestUri]()
-    //             }
-    //         }
-    //
-    //     }
-    //
-    //
-    // }
-    //
-    // // Call this method when module is loaded
-    // Module.prototype.onload = function () {
-    //     var mod = this
-    //     mod.status = STATUS.LOADED
-    //
-    //     if (mod.callback) {
-    //         mod.callback()
-    //     }
-    //
-    //     // Notify waiting modules to fire onload
-    //     var waitings = mod._waitings
-    //     var uri, m
-    //
-    //     for (uri in waitings) {
-    //         if (waitings.hasOwnProperty(uri)) {
-    //             m = cachedMods[uri]
-    //             m._remain -= waitings[uri]
-    //             if (m._remain === 0) {
-    //                 m.onload()
-    //             }
-    //         }
-    //     }
-    //
-    //     // Reduce memory taken
-    //     delete mod._waitings
-    //     delete mod._remain
-    // }
-    //
-    // // Fetch a module
-    // // fetch 实际上只是创建了请求, 保存在 requestCache, 请求是在 load 从 中发送的
-    // Module.prototype.fetch = function (requestCache) {
-    //     var mod = this
-    //     var uri = mod.uri
-    //
-    //     // 更新状态
-    //     mod.status = STATUS.FETCHING
-    //
-    //     // Emit `fetch` event for plugins such as combo plugin
-    //     var emitData = { uri: uri }
-    //     emit('fetch', emitData)
-    //
-    //     var requestUri = emitData.requestUri || uri
-    //
-    //     // Empty uri or a non-CMD module
-    //     // 空 uri 或者 非 cmd 模块, 或者 模块已 fetch
-    //     if (!requestUri || fetchedList[requestUri]) {
-    //         mod.load()
-    //         return
-    //     }
-    //
-    //     // 正在 fetch
-    //     if (fetchingList[requestUri]) {
-    //         callbackList[requestUri].push(mod)
-    //         return
-    //     }
-    //
-    //     // fetch
-    //     fetchingList[requestUri] = true
-    //     callbackList[requestUri] = [mod]
-    //
-    //     emitData = {
-    //         uri: uri,
-    //         requestUri: requestUri,
-    //         onRequest: onRequest,
-    //         charset: isFunction(data.charset) ? data.charset(requestUri) : data.charset,
-    //         crossorigin: isFunction(data.crossorigin) ? data.crossorigin(requestUri) : data.crossorigin
-    //     }
-    //
-    //     // Emit `request` event for plugins such as text plugin
-    //     emit('request', emitData)
-    //
-    //     //
-    //     if (!emitData.requested) {
-    //         // requestCache 第一次是 {}
-    //         if(requestCache) {
-    //             requestCache[emitData.requestUri] = sendRequest
-    //         } else {
-    //             sendRequest()
-    //         }
-    //     }
-    //
-    //     function sendRequest () {
-    //         seajs.request(emitData.requestUri, emitData.onRequest, emitData.charset, emitData.crossorigin)
-    //     }
-    //
-    //     // fetch 完成的回调
-    //     function onRequest () {
-    //         delete fetchingList[requestUri]
-    //         fetchedList[requestUri] = true
-    //
-    //         // Save meta data of anonymous module
-    //         if (anonymousMeta) {
-    //             Module.save(uri, anonymousMeta)
-    //             anonymousMeta = null
-    //         }
-    //
-    //         // 依赖模块加载完成, 加载当前模块
-    //         var m, mods = callbackList[requestUri]
-    //         delete callbackList[requestUri]
-    //         while ((m = mods.shift())) {
-    //             m.load()
-    //         }
-    //     }
-    // }
-    //
-    // // Execute a module
-    // Module.prototype.exec = function () {
-    //     var mod = this
-    //
-    //     // When module is executed, DO NOT execute it again. When module
-    //     // is being executed, just return `module.exports` too, for avoiding
-    //     // circularly calling
-    //     if (mod.status >= STATUS.EXECUTING) {
-    //         return mod.exports
-    //     }
-    //
-    //     mod.status = STATUS.EXECUTING
-    //
-    //     // Create require
-    //     var uri = mod.uri
-    //
-    //     function require (id) {
-    //         return Module.get(require.resolve(id)).exec()
-    //     }
-    //
-    //     require.resolve = function (id) {
-    //         return Module.resolve(id, uri)
-    //     }
-    //
-    //     require.async = function (ids, callback) {
-    //         Module.use(ids, callback, uri + '_async_' + cid())
-    //         return require
-    //     }
-    //
-    //     // Exec factory
-    //     var factory = mod.factory
-    //
-    //     var exports = isFunction(factory) ? factory(require, mod.exports = {}, mod) : factory
-    //
-    //     if (exports === undefined) {
-    //         exports = mod.exports
-    //     }
-    //
-    //     // Reduce memory leak
-    //     delete mod.factory
-    //
-    //     mod.exports = exports
-    //     mod.status = STATUS.EXECUTED
-    //
-    //     // Emit `exec` event
-    //     emit('exec', mod)
-    //
-    //     return exports
-    // }
-    //
-    // // 将 模块的 id 转换为 uri
-    // Module.resolve = function (id, refUri) {
-    //     // Emit `resolve` event for plugins such as text plugin
-    //     var emitData = { id: id, refUri: refUri }
-    //     emit('resolve', emitData)
-    //
-    //     return emitData.uri || seajs.resolve(emitData.id, refUri)
-    // }
-    //
-    // // Define a module
-    // Module.define = function (id, deps, factory) {
-    //     var argsLen = arguments.length
-    //
-    //     // define(factory)
-    //     if (argsLen === 1) {
-    //         factory = id
-    //         id = undefined
-    //     }
-    //     else if (argsLen === 2) {
-    //         factory = deps
-    //
-    //         // define(deps, factory)
-    //         if (isArray(id)) {
-    //             deps = id
-    //             id = undefined
-    //         }
-    //         // define(id, factory)
-    //         else {
-    //             deps = undefined
-    //         }
-    //     }
-    //
-    //     // Parse dependencies according to the module factory code
-    //     if (!isArray(deps) && isFunction(factory)) {
-    //         deps = parseDependencies(factory.toString())
-    //     }
-    //
-    //     var meta = {
-    //         id: id,
-    //         uri: Module.resolve(id),
-    //         deps: deps,
-    //         factory: factory
-    //     }
-    //
-    //     // Try to derive uri in IE6-9 for anonymous modules
-    //     if (!meta.uri && doc.attachEvent) {
-    //         var script = getCurrentScript()
-    //
-    //         if (script) {
-    //             meta.uri = script.src
-    //         }
-    //
-    //         // NOTE: If the id-deriving methods above is failed, then falls back
-    //         // to use onload event to get the uri
-    //     }
-    //
-    //     // Emit `define` event, used in nocache plugin, seajs node version etc
-    //     emit('define', meta)
-    //
-    //     meta.uri ? Module.save(meta.uri, meta) : // Save information for "saving" work in the script onload event
-    //       anonymousMeta = meta
-    // }
-    //
-    // // Save meta data to cachedMods
-    // Module.save = function (uri, meta) {
-    //     var mod = Module.get(uri)
-    //
-    //     // Do NOT override already saved modules
-    //     if (mod.status < STATUS.SAVED) {
-    //         mod.id = meta.id || uri
-    //         mod.dependencies = meta.deps || []
-    //         mod.factory = meta.factory
-    //         mod.status = STATUS.SAVED
-    //     }
-    // }
-    //
-    // // 获取已存在的数组, 如果模块不存在则创建新模块
-    // // deps 是数组
-    // Module.get = function (uri, deps) {
-    //     const mod = cachedMods[uri]
-    //     if(mod) {
-    //         return mod
-    //     } else {
-    //         const newMod = new Module(uri, deps)
-    //         cachedMods[uri] = newMod
-    //         return newMod
-    //     }
-    // }
-    //
-    // // Use function is equal to load a anonymous module
-    // Module.use = function (ids, callback, uri) {
-    //     // 获取模块
-    //     const mod = Module.get(uri, isArray(ids) ? ids : [ids])
-    //
-    //     // 在 onload 中调用
-    //     mod.callback = function () {
-    //         var exports = []
-    //         var uris = mod.resolve()
-    //
-    //         for (var i = 0, len = uris.length; i < len; i++) {
-    //             exports[i] = cachedMods[uris[i]].exec()
-    //         }
-    //
-    //         if (callback) {
-    //             callback.apply(global, exports)
-    //         }
-    //
-    //         delete mod.callback
-    //     }
-    //
-    //     mod.load()
-    // }
-    //
-    // // 加载所有预加载模块
-    // Module.preload = function (callback) {
-    //     console.log('step3: preload')
-    //
-    //     var preloadMods = data.preload
-    //     var len = preloadMods.length
-    //
-    //     if (len) {
-    //         Module.use(preloadMods, function () {
-    //             // Remove the loaded preload modules
-    //             preloadMods.splice(0, len)
-    //
-    //             // Allow preload modules to add new preload modules
-    //             Module.preload(callback)
-    //         }, data.cwd + '_preload_' + cid())
-    //     } else {
-    //         callback()
-    //     }
-    // }
-    
-    // Public API
-    seajs.use = function (ids, callback) {
-        console.log('step2: seajs.use', data.cwd, cid())
-        
-        Module.preload(function () {
-            console.log('step4: load main script')
-            Module.use(ids, callback, data.cwd + '_use_' + cid())
-        })
-        return seajs
-    }
     
     Module.define.cmd = {}
     
-    global.define = Module.define
     
-    // For Developers
     
-    seajs.Module = Module
+    
     // TODO
     data.fetchedList = Module.fetchedList
     data.cid = cid
     
-    seajs.require = function (id) {
-        var mod = Module.get(Module.resolve(id))
-        if (mod.status < STATUS.EXECUTING) {
-            mod.onload()
-            mod.exec()
-        }
-        return mod.exports
-    }
+    
     
     /**
      * config.js - The configuration for the loader
@@ -1352,6 +951,25 @@
     data.debug = false
     // 初始化 data 结束
     
+    // Public API
+    seajs.use = function (ids, callback) {
+        console.log('step2: seajs.use', data.cwd, cid())
+        
+        Module.preload(function () {
+            console.log('step4: load main script')
+            Module.use(ids, callback, data.cwd + '_use_' + cid())
+        })
+        return seajs
+    }
+    seajs.Module = Module
+    seajs.require = function (id) {
+        var mod = Module.get(Module.resolve(id))
+        if (mod.status < STATUS.EXECUTING) {
+            mod.onload()
+            mod.exec()
+        }
+        return mod.exports
+    }
     
     // config api
     // 依赖 addBase 方法, 必须在 data 初始化之后
@@ -1394,5 +1012,8 @@
         emit('config', configData)
         return seajs
     }
+    
+    global.define = Module.define
+    global.seajs = seajs
     
 })(this)
